bbe0dd283d34b6ee58f9e6175601d219f4cb3c40
Alireza Saadatnia <alirezasn3@gmail.com>
2024-10-17 22:15:59 +0330

    improved bypass logic

--- Middleware.go
+++ Middleware.go
@@ -9,13 +9,15 @@ import (
 
 func Auth(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(ctx echo.Context) error {
+		// check for admin bypass
+		if ctx.Request().Header.Get("bypass_key") == config.BypassKey {
+			ctx.Set("bypass", true)
+			return next(ctx)
+		}
+
 		ip := strings.Split(ctx.Request().RemoteAddr, ":")[0]
 		// check if request is from peer
 		if !deviceCIDR.Contains(net.ParseIP(ip)) {
-			if ctx.Request().Header.Get("bypass_key") == config.BypassKey {
-				ctx.Set("bypass", true)
-				return next(ctx)
-			}
 			logger.Warn("Unauthorized request from " + ctx.Request().RemoteAddr)
 			return ctx.NoContent(403)
 		}
